552b58ebedd2c8bff4bb6765c98195aeb5872c13
Kuang Hsuan Lin <samcool199753@gmail.com>
2020-10-18 21:16:53 -0700

    lab1 finished

--- Lab1/lab1.h
+++ Lab1/lab1.h
@@ -2,6 +2,11 @@
 #define __HEADER1_H__
 
 #define RCGCGPIO (*((volatile uint32_t *)0x400FE608))
+#define Port_F 0x20
+#define Port_N 0x1000
+#define Port_J 0x100
+#define Port_C 0x04
+#define Port_L 0x400
 
 // Port F Address
 #define GPIODIR_F (*((volatile uint32_t *)0x4005D400))
@@ -13,27 +18,85 @@
 #define GPIODEN_N (*((volatile uint32_t *)0x4006451C))
 #define GPIODATA_N (*((volatile uint32_t *)0x400643FC))
 
+#define GPIOAMSEL_N (*((volatile uint32_t *)0x40064528))
+#define GPIOAFSEL_N (*((volatile uint32_t *)0x40064420))
+
 // Port J Address
 #define GPIODEN_J (*((volatile uint32_t *)0x4006051C))
 #define GPIODIR_J (*((volatile uint32_t *)0x40060400))
 #define GPIOPUR_J (*((volatile uint32_t *)0x40060510))
 #define GPIODATA_J (*((volatile uint32_t *)0x400603FC))
 
-#define Port_F 0x20
-#define Port_N 0x1000
-#define Port_J 0x100
+// Port C Address
+#define GPIODIR_C (*((volatile uint32_t *)0x4005A400))
+#define GPIODEN_C (*((volatile uint32_t *)0x4005A51C))
+#define GPIODATA_C (*((volatile uint32_t *)0x4005A3FC))
+
+#define GPIOAMSEL_C (*((volatile uint32_t *)0x4005A528))
+#define GPIOAFSEL_C (*((volatile uint32_t *)0x4005A420))
+
+// Port L Address
+#define GPIODIR_L (*((volatile uint32_t *)0x40062400))
+#define GPIODEN_L (*((volatile uint32_t *)0x4006251C))
+#define GPIODATA_L (*((volatile uint32_t *)0x400623FC))
+
+#define GPIOAMSEL_L (*((volatile uint32_t *)0x40062528))
+#define GPIOAFSEL_L (*((volatile uint32_t *)0x40062420))
+
+// Port E Address
+#define GPIODIR_E (*((volatile uint32_t *)0x4005C400))
+#define GPIODEN_E (*((volatile uint32_t *)0x4005C51C))
+#define GPIODATA_E (*((volatile uint32_t *)0x4005C3FC))
+
+#define GPIOAMSEL_E (*((volatile uint32_t *)0x4005C528))
+#define GPIOAFSEL_E (*((volatile uint32_t *)0x4005C420))
+
+// Port M Address
+#define GPIODIR_M (*((volatile uint32_t *)0x40063400))
+#define GPIODEN_M (*((volatile uint32_t *)0x4006351C))
+#define GPIODATA_M (*((volatile uint32_t *)0x400633FC))
+
+#define GPIOAMSEL_M (*((volatile uint32_t *)0x40063528))
+#define GPIOAFSEL_M (*((volatile uint32_t *)0x40063420))
 
 // Port F Control
 #define PF0 0x01 // connect to LED4
 #define PF4 0x10 // connect to LED3
+
 // Port N Control
 #define PN0 0x1 // connect to LED2
 #define PN1 0x2 // connect to LED1
+#define PN2 0x4
+
 // Port J Control
 #define PJ0 0x1 // connect to SW1
 #define PJ1 0x2 // connect to SW2
 
-// LED
+// Port C Control
+#define PC4 0x10
+
+// Port L Control
+#define PL0 0x1  // connect to System ON/OFF button
+#define PL1 0x2  // connect to Pedestrian ON/OFF button
+#define PL2 0x4  // connect to Red LED
+#define PL3 0x8  // connect to Yellow LED
+#define PL4 0x10 // connect to Green LED
+
+// Port E Control
+#define PE0 0x1
+#define PE1 0x2
+#define PE2 0x4
+#define PE3 0x8
+#define PE5 0x20
+
+// Port M Control
+#define PM0 0x1
+#define PM1 0x2
+#define PM2 0x4
+#define PM6 0x40
+#define PM7 0x80
+
+// OnBoard LED
 #define LED1 0x2
 #define LED2 0x1
 #define LED3 0x10
---
+++ Lab1/task2_trafficLight.c
@@ -0,0 +1,192 @@
+#include <stdint.h>
+#include "lab1.h"
+
+void LED_init(void)
+{
+  volatile unsigned short delay = 0;
+
+  // Enable Port L Gating Clock
+  RCGCGPIO |= Port_L;
+  delay++;
+  delay++;
+  // Disable PL2, PL3, PL4 analog function
+  GPIOAMSEL_L &= ~PL2;
+  GPIOAMSEL_L &= ~PL3;
+  GPIOAMSEL_L &= ~PL4;
+  // Select PN2PL2, PL3, PL4 regular port function
+  GPIOAFSEL_C &= ~PL2;
+  GPIOAFSEL_C &= ~PL3;
+  GPIOAFSEL_C &= ~PL4;
+  ​// Set PL2, PL3, PL4 to input direction
+  GPIODIR_L |= PL2;
+  GPIODIR_L |= PL3;
+  GPIODIR_L |= PL4;
+  ​// Enable PL2, PL3, PL4 digital function
+  GPIODEN_L |= PL2;
+  GPIODEN_L |= PL3;
+  GPIODEN_L |= PL4;
+}
+// turn on Green LED
+void Go_State(void)
+{
+  GPIODATA_L &= ~PL2;
+  GPIODATA_L &= ~PL3;
+  GPIODATA_L |= PL4;
+}
+// turn on Red LED
+void Stop_State(void)
+{
+  GPIODATA_L |= PL2;
+  GPIODATA_L &= ~PL3;
+  GPIODATA_L &= ~PL4;
+}
+// turn on Yellow LED
+void Warn_State(void)
+{
+  GPIODATA_L &= ~PL2;
+  GPIODATA_L |= PL3;
+  GPIODATA_L &= ~PL4;
+}
+// turn off all LEDs
+void LED_off(void)
+{
+  GPIODATA_L &= ~PL2;
+  GPIODATA_L &= ~PL3;
+  GPIODATA_L &= ~PL4;
+}
+
+void extern_switch_init(void)
+{
+  volatile unsigned short delay = 0;
+  RCGCGPIO |= Port_L;
+  delay++;
+  delay++;
+  // Disable PL0, PL1 analog function
+  GPIOAMSEL_L &= ~PL0;
+  GPIOAMSEL_L &= ~PL1;
+  // Select PL0, PL1 regular port function
+  GPIOAFSEL_L &= ~PL0;
+  GPIOAFSEL_L &= ~PL1;
+  // Set PL0, PL1 to input direction
+  GPIODIR_L &= ~PL0;
+  GPIODIR_L &= ~PL1;
+  // Enable PL0, PL1 digital function
+  GPIODEN_L |= PL0;
+  GPIODEN_L |= PL1;
+}
+unsigned long sys_switch(void)
+{
+  return (GPIODATA_L & PL0);
+}
+
+unsigned long ped_switch(void)
+{
+  return (GPIODATA_L & PL1);
+}
+
+int time = 0;
+enum TFF_State
+{
+  TFF_Begin,
+  TFF_S0,
+  TFF_S1,
+  TFF_S2,
+  TFF_End
+} TFF_State;
+void tffFunc()
+{
+  switch (TFF_State)
+  {
+  case TFF_Begin:
+    TFF_State = TFF_S0;
+    break;
+  case TFF_S0:
+    if (sys_switch())
+    {
+      TFF_State = TFF_End;
+    }
+    else if (ped_switch())
+    {
+      TFF_State = TFF_S1;
+      time = 0;
+    }
+    else if (time == 500000)
+    {
+      TFF_State = TFF_S2;
+      time = 0;
+    }
+    else
+    {
+      time++;
+    }
+    break;
+  case TFF_S1:
+    if (sys_switch())
+    {
+      TFF_State = TFF_End;
+    }
+    else if (time == 250000)
+    {
+      TFF_State = TFF_S2;
+      time = 0;
+    }
+    else
+    {
+      time++;
+    }
+    break;
+  case TFF_S2:
+    if (sys_switch())
+    {
+      TFF_State = TFF_End;
+    }
+    else if (time == 1000000)
+    {
+      TFF_State = TFF_S0;
+      time = 0;
+    }
+    else
+    {
+      time++;
+    }
+    break;
+  case TFF_End:
+    LED_off();
+    time = 0;
+    if (sys_switch())
+    {
+      TFF_State = TFF_S0;
+    }
+    break;
+  default:
+    TFF_State = TFF_Begin;
+    break;
+  }
+  switch (TFF_State)
+  {
+  case TFF_S0:
+    Go_State();
+    break;
+  case TFF_S1:
+    Warn_State();
+    break;
+  case TFF_S2:
+    Stop_State();
+    break;
+  case TFF_End:
+    LED_off();
+    break;
+  default:
+    break;
+  }
+}
+int main()
+{
+  LED_init();
+  extern_switch_init();
+  while (1)
+  {
+    tffFunc();
+  }
+  return 0;
+}
